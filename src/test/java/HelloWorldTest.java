62677eef8bdcfe9e6a9f1f4bb2a01fea54115e77
Zhikina <Swan2489@gmail.com>
2026-07-20 14:25:09 +0300

    EX4

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
 
-        System.out.println("Hello from Женя");
     }
 }
