f7e6e8148966bb7239cf74e0b85b7b76a7f89fd9
Zhikina <Swan2489@gmail.com>
2026-07-29 16:47:05 +0300

    EX_5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,11 +1,6 @@
-import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.http.Headers;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class HelloWorldTest {
