6a456c913b9e93083848fa8e766ee5cd9178641c
Zhikina <Swan2489@gmail.com>
2026-07-29 16:41:08 +0300

    EX_5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,15 +1,23 @@
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.http.Headers;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
-         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
-        response.prettyPrint();
 
+        Response response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .andReturn();
+
+        String message = response.jsonPath().getString("messages[1].message");
+        System.out.println("Ответ: " + message);
     }
 }
