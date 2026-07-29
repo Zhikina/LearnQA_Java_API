9b6238c5923e4b99a77736ff51154d1a10c471c8
Zhikina <Swan2489@gmail.com>
2026-07-29 18:08:28 +0300

    EX_8

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,27 +2,49 @@ import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld() {
-        String testUrl = "https://playground.learnqa.ru/api/long_redirect";
-    while (true){
+    public void testHelloWorld()  throws InterruptedException {
+        String testUrl = "https://playground.learnqa.ru/api/longtime_job";
+
         Response response = RestAssured
                 .given()
-                .redirects()
-                .follow(false)
-                .when()
                 .get(testUrl)
                 .andReturn();
 
-        if (response.getStatusCode() == 200) {
-            System.out.println("Ответ: " + testUrl);
-            break;
-        }
-        String newTestUrl = response.getHeader("Location");
-        testUrl = newTestUrl;
-    }
+        String token = response.jsonPath().getString("token");
+        int seconds = response.jsonPath().getInt("seconds");
+
+
+        Response notReadyResponse = RestAssured
+                .given()
+                .queryParam("token", token)
+                .get(testUrl)
+                .andReturn();
+
+
+        String statusBefore = notReadyResponse.jsonPath().getString("status");
+        assertEquals("Job is NOT ready", statusBefore);
+
+        Thread.sleep(seconds * 1000L);
+
+        Response readyResponse = RestAssured
+                .given()
+                .queryParam("token", token)
+                .get(testUrl)
+                .andReturn();
+
+
+        String statusAfter = readyResponse.jsonPath().getString("status");
+        assertEquals("Job is ready", statusAfter);
+
+
+        String result = readyResponse.jsonPath().getString("result");
+        assertNotNull(result, "Поле result не должно быть null");
 
     }
 }
