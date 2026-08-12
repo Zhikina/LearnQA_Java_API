3731d1fe319a07e03c14856350cea8f4ce6276d4
Zhikina <Swan2489@gmail.com>
2026-08-12 08:28:54 +0300

    Ex10

---
+++ src/test/java/tests/testLength.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class testLength {
+
+    @Test
+    public void testStringLength() {
+        String hello = "Hello, world! This is long enough";
+        int length = hello.length();
+        assertTrue(length > 15,
+                "Длина строки (" + length + ") меньше или равна 15. Текст: \"" + hello + "\"");
+    }
+}
