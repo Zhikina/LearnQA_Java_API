62f580b5eb1a6426dbddd755075a52c503f60c36
Zhikina <Swan2489@gmail.com>
2026-08-17 19:56:07 +0300

    ex15

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -2,15 +2,19 @@ package tests;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.Assertions;
 import lib.BaseTestCase;
 import lib.DataGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserRegisterTest extends BaseTestCase {
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
     @Test
     public void testCreateUserWithExistingEmail(){
         String email = "vinkotov@example.com";
@@ -53,6 +57,71 @@ public class UserRegisterTest extends BaseTestCase {
 
 
     }
+
+    // 1. Создание пользователя с некорректным email (без символа @)
+    @Test
+    public void testCreateUserWithInvalidEmail() {
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+        userData.put("email", "invalidEmailWithoutAtSymbol.com");
+
+        Response response = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response, "Invalid email format");
+
+    }
+    // 2. Создание пользователя без одного из обязательных полей (параметризованный тест)
+    @ParameterizedTest
+    @ValueSource(strings = {"email", "password", "username", "firstName", "lastName"})
+    public void testCreateUserWithoutRequiredField(String missingField) {
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+        userData.remove(missingField);
+
+        Response response = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response,
+                "The following required params are missed: " + missingField);
+
+    }
+
+    // 3. Создание пользователя с именем из одного символа
+    @Test
+    public void testCreateUserWithShortName() {
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+        userData.put("firstName", "A");
+
+        Response response = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response,
+                "The value of 'firstName' field is too short");
+
+    }
+
+    // 4. Создание пользователя с именем длиннее 250 символов
+    @Test
+    public void testCreateUserWithVeryLongName() {
+        String longName = "a".repeat(251); // 251 символ
+
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+        userData.put("firstName", longName);
+
+        Response response = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(response, 400);
+        Assertions.assertResponseTextEquals(response,
+                "The value of 'firstName' field is too long");
+
+    }
+
+
+
+
 
 
 }
